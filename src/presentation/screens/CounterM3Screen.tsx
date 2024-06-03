b200d23804bf517a55611a9a45cb3eccfb681717
emanuelhardwell <emanuelnoe16@gmail.com>
2024-06-03 14:54:56 -0600

    feat(deps): install react-native-vector-icons and update android/app/build.gradle

--- src/presentation/screens/CounterM3Screen.tsx
+++ src/presentation/screens/CounterM3Screen.tsx
@@ -3,6 +3,7 @@ import {Text, View} from 'react-native';
 // import {PrimaryButton} from '../components/shared/PrimaryButton';
 import {FAB} from 'react-native-paper';
 import {globalStyles} from '../theme/global.styles';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export const CounterM3Screen = () => {
   const [counter, setCounter] = useState<number>(10);
@@ -10,11 +11,13 @@ export const CounterM3Screen = () => {
   return (
     <View style={globalStyles.containerCenter}>
       <Text style={globalStyles.title}> {counter} </Text>
+      <Icon name="accessibility-outline" size={25} />
       <FAB
         label="+1"
         style={globalStyles.fab}
         onPress={() => setCounter(counter + 1)}
         onLongPress={() => setCounter(0)}
+        icon={() => <Icon name="accessibility-outline" size={25} />}
       />
     </View>
   );
