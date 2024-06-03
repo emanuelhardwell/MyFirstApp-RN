ba72a4211bb9924ab0c8f0ee30214498f78e40e1
emanuelhardwell <emanuelnoe16@gmail.com>
2024-06-03 13:41:50 -0600

    feat(deps): Install UI library react-native-paper and use its components

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
 // import {HelloWorldScreen} from './src/presentation/screens/HelloWorldScreen';
-import { CounterScreen } from './src/presentation/screens/CounterScreen';
+import {CounterScreen} from './src/presentation/screens/CounterScreen';
+import {PaperProvider} from 'react-native-paper';
 
 export const App = () => {
   return (
-    <SafeAreaView style={{flex: 1}}>
-      {/* <HelloWorldScreen /> */}
-      <CounterScreen />
-    </SafeAreaView>
+    <PaperProvider>
+      <SafeAreaView style={{flex: 1}}>
+        {/* <HelloWorldScreen /> */}
+        <CounterScreen />
+      </SafeAreaView>
+    </PaperProvider>
   );
 };
